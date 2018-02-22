3e1cade3fe1e1d52606c76b7de99450439a87d60
Victor Lava <hello@victorlava.com>
2018-02-22 17:46:13 +0200

    returned thumbnail image for featured reviews

--- index.php
+++ index.php
@@ -45,14 +45,14 @@
                   <?php echo $review->l_name ?>
               </strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#"><?php echo $review->email; ?></a>
+                <a class="text-dark" href="#"><?php echo Helper::str_limit($review->review, 18); ?></a>
               </h3>
               <div class="mb-1 text-muted"><?php echo Helper::format_date($review->date); ?></div>
               <p class="card-text mb-auto">
                   <?php echo Helper::str_limit($review->review, 150); ?>
               </p>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block"  alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3csvg width='400' height='400' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 400 400' %3e%3crect x='0' y='0' width='120' height='120' fill='black' opacity='1'%3e%3c/rect%3e%3ctext x='100' y='100' font-family='Verdana' font-size='35' fill='red'%3eTekstukas%3c/text%3e%3c/svg%3e" data-holder-rendered="true">
+            <img class="card-img-right flex-auto d-none d-md-block"  alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_161be3054cb%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_161be3054cb%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2255.890625%22%20y%3D%22130.7234375%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
           </div>
         </div>
         <?php endforeach; ?>
